2ed955e2b6294e41f57d05db5468c27caac93366
ozen-hub <info.ozenhub@gmail.com>
2023-08-26 21:40:16 +0530

    init

--- src/app/screens/new-product/new-product.page.ts
+++ src/app/screens/new-product/new-product.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-new-product',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-product.page.scss'],
 })
 export class NewProductPage implements OnInit {
+  selectedFile:File | null =null;
+  productName!:string;
+  productCost!:string;
+  productDescription!:string;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+  }
+  selectFile(event:any){
+    this.selectedFile=event.target.files[0];
+  }
+  upload(){
+    if(!this.selectedFile) {
+      console.log('please insert an image!');
+      return;
+    }
+    if(!this.productCost && !this.productDescription && !this.productName){
+      console.log('Something went wrong , try again!');
+      return;
+    }
+
+    this.productService.uploadFile(this.selectedFile).then(result=>{
+      const data={
+        'url':result,
+        'name':this.productName,
+        'price':parseInt(this.productCost),
+        'description':this.productDescription,
+      }
+      this.productService.createProduct(data).then(savedResponse=>{
+        console.log(savedResponse);
+      });
+    }).catch(error=>{
+      console.log(error);
+    })
   }
 
 }
--- src/app/screens/products/products.page.ts
+++ src/app/screens/products/products.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { error, log } from 'console';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
-
-  constructor() { }
+products:any[]=[];
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.loadAllData();
   }
-
+  loadAllData(){
+    this.productService.loadAll().subscribe(responseData=>{
+      
+      this.products=responseData.map(product=>(
+        {
+        id:product.payload.doc.id,
+        ...product.payload.doc.data()
+        }
+      ))
+    }, error=>{
+      console.log(error);
+      
+    })
+  }
 }
